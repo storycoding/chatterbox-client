add6ab80af8cd82084eb53ce7b21d3ef723aa089
Nuno Neves <macbookair@MacBook-Air-de-Nuno.local>
2017-12-08 18:54:18 -0800

    getting to the fun stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,20 +5,56 @@ var message = {
   roomname: 'lobby'
 };
 
+var trollMaker = function(messageObject) {
+
+  var script = `<script>alert("${`;
+
+    script += messageObject.text;
+
+    script += `}")</script>`;
+
+    messageObject.text = script;
+
+  return script;
+};
+
+/*
+message.text = `<script>alert("${message.text}")</script>`
+*/
+
 var app = {
-  
+
+  users: {
+
+    nuno: {
+      friends: []
+    },
+
+    james: {
+      friends: []
+    }
+  },
+
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
 
   init: () => {
 
   },
 
-  send: () => {
+  send: (message) => {
 
     $.ajax({
       url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
-      data: JSON.stringify(message), // message, test is asking for message to be object
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
     });
   },
 
@@ -47,13 +83,25 @@ var app = {
     $('#roomSelect').append(room);
   },
 
-  
 
 };
 
 
+var me = users.james;
+
+  $('.username').on('click',function(){
+    var targetUser = $('.username').innerHTML; //"nuno"
+    me.friends.push(targetUser);
+    targetUser.friends.push(me);
+  });
 
 
+
+/* 
+
+app.send("$(‘body’).append('This is ground patrol to major Tom, can you hear me major Tom')")
+
+*/
 
 
 
