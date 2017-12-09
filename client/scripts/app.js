7c6c16e15fc7bb042b734a4bf609fe3d0823e13c
storycoding <youknownuno@gmail.com>
2017-12-09 12:40:58 -0800

    Complete render message function and escape attach messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,3 @@
-// YOUR CODE HERE:
-
 $(document).ready(function() {
   
 
@@ -14,17 +12,24 @@ $(document).ready(function() {
       targetUser.friends.push(me);
       
       console.log(targetUser);
+    });
+    
+    $('.submit').on('click', function() {
+   
+      var message = createMessage();
+      console.log(message);
+      //app.send(message);
+    
     });  
 
   };
-
 
 
   assignButtons();
 
 });
 
-var stuff;
+var messageLog;
 
 var message = {
   username: 'Mel Brooks',
@@ -43,9 +48,7 @@ var trollMaker = function(messageObject) {
   return script;
 };
 
-/*
-message.text = `<script>alert("${message.text}")</script>`
-*/
+
 
 var app = {
 
@@ -62,9 +65,11 @@ var app = {
 
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
 
+
   init: () => {
 
   },
+
 
   send: (message) => {
 
@@ -83,14 +88,53 @@ var app = {
     });
   },
 
+
   fetch: () => {
-    $.get('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', function(data) {
+      
+    $.get('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages', {order: '-createdAt'}, function(data) {
       //var parsedData = JSON.parse(data);
       //console.dir(parsedData);
       console.log(data);
-      stuff = data;
+      messageLog = data;
+      
+      var messageObjectArray = messageLog.results;
+      
+      
+      var attacksDodged = 0;
+      //iterate through each message from messageLog
+      for (var i = messageObjectArray.length - 1; i >= 0; i--) {
+        
+        if (messageObjectArray[i] === undefined) {
+          continue;
+        }
+        
+        
+        if (messageObjectArray[i].text === undefined) {
+          continue;
+        }
+        //if (messageObjectArray[i].text.charAt(0) === '<') { // starts with script
+        if ((/[-%[\]{}<>()*+?.,\\^$|#\s]/g).test(messageObjectArray[i].text)) {
+          attacksDodged++;
+          delete messageObjectArray[i];
+          continue;
+        }
+            
+        if (messageObjectArray[i].username === undefined) {
+          continue;
+        }
+        //if (messageObjectArray[i].username.charAt(0) === '<') { // starts with script
+        if ((/[-%[\]{}<>()*+?.,\\^$|#\s]/g).test(messageObjectArray[i].username)) {
+          attacksDodged++;
+          delete messageObjectArray[i];
+        }
+        app.renderMessage(messageObjectArray[i]);
+      }
+      
+      
+      console.log('Attacks Dodged: ', attacksDodged);
       alert('message loaded successfully');
     });
+    
     
   },
 
@@ -105,12 +149,18 @@ var app = {
     var $userClass = $('<span class="username"><span>');
     $userClass.text(message.username);
 
-    console.log($userClass.text);
+    var $colon = $('<span>: <span>');
     
-    //userMessage.text(userText);
-
-
-    $('#chats').append(userMessage);
+    var $userMessageText = $('<span class="messageText"></span>');
+    $userMessageText.text(message.text);
+  
+    // append username, colon, and message to .messageBox div
+    $userMessage.append($userClass);
+    $userMessage.append($colon);
+    $userMessage.append($userMessageText);
+    
+    // append .messageBox div to #chats div
+    $('#chats').append($userMessage);
   },
 
   renderRoom: () => {
@@ -122,46 +172,62 @@ var app = {
   },
 
 
+
+  refresh: () => {
+    //fetch() // defines messageLog
+    
+    app.fetch(); // establish a promise //fetched results might not be ready yet
+    
+    //more recent ones first / twitter style
+    
+    var messageObjectArray = messageLog.results;
+    
+    //iterate through each message from messageLog
+    for (var i = messageObjectArray.length - 1; i >= 0; i--) {
+      app.renderMessage(messageObjectArray[i]);
+    }
+
+  }
+
 };
 
+sendMessage: () => {
+  
+  
+};
+
+var createMessage = function() {
+  var $textBox = $('.messageInput');
+  
+  var messageObject = {};
+  messageObject.text = $textBox[0].value;
+  messageObject.username = me.username; /////////////////// me.username needs to be the popup name
+  messageObject.roomname = 'lobby'; /////////////////// update when rooms get created
+
+  return messageObject;
+};
+
+// create a function that:
+  //transform input from textbox into a message object
+    //message.text = inputText
+    //message.username = our username
+    //message.roomname = current room // work on this
+
+// when the send button is pushed
+// make an AJAX send request
+
+//WIP
 var me = app.users.james;
 
 
 var logAll = function () {
-  for(var i = 0; i < stuff.results.length-1; i++) {
+  for (var i = 0; i < stuff.results.length - 1; i++) {
     console.log(stuff.results[i].text);
   } 
-}
-
-/* 
-
-app.send("$(‘body’).append('This is ground patrol to major Tom, can you hear me major Tom')")
-
-
-
-
-
-
-
-*/
-
-
+};
 
 /*
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message), // message, test is asking for message to be object
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-
+message.text = `<script>alert("${message.text}")</script>`
 */
+
+//app.send("$(‘body’).append('This is ground patrol to major Tom, can you hear me major Tom')")
